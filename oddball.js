b470cb18b6e9afc626738d0b68657c6d7d2b8569
Candice Thomason <candice.thomason@gmail.com>
2024-04-06 18:23:23 +1100

    home team in

---
+++ oddball.js
@@ -0,0 +1,66 @@
+function gameObject() {}
+const teams = {
+  home: {
+    teamName: "Brookly Nets",
+    colors: ["Black", "White"],
+    players: [
+      {
+        name: "Alan anderson",
+        number: "0",
+        shoe: "16",
+        points: "22",
+        rebounds: "12",
+        assists: "12",
+        blocks: "1",
+        steals: "3",
+        slamDunks: "1",
+      },
+      {
+        name: "Reggie Evans",
+        number: "30",
+        shoe: "14",
+        points: "12",
+        rebounds: "12",
+        assists: "12",
+        blocks: "12",
+        steals: "12",
+        slamDunks: "1",
+      },
+      {
+        name: "Brook Lopez",
+        number: "11",
+        shoe: "17",
+        points: "17",
+        rebounds: "19",
+        assists: "10",
+        steals: "3",
+        blocks: "1",
+        slamDunks: "15",
+      },
+      {
+        name: "Mason Plumlee",
+        number: "1",
+        shoe: "19",
+        points: "26",
+        rebounds: "12",
+        assists: "6",
+        steals: "3",
+        blocks: "8",
+        slamDunks: "5",
+      },
+      {
+        name: "Jason Terry",
+        number: "31",
+        shoe: "15",
+        points: "19",
+        rebounds: "2",
+        assists: "2",
+        steals: "4",
+        blocks: "11",
+        slamDunks: "1",
+      },
+    ],
+  },
+};
+
+console.log(teams);
